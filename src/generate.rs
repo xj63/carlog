5d081435eaff40c34e6ace32bfe5c527a6d043ab
xj63 <xj63work@163.com>
2024-07-10 15:06:36 +0800

    feat(generate): add generate shell complete

--- src/generate.rs
+++ src/generate.rs
@@ -1,18 +1,23 @@
 use crate::cli::Cli;
-use clap::Args;
-use clap::CommandFactory;
-use clap::ValueEnum;
+use clap::{Args, CommandFactory, ValueEnum};
+use clap_complete::{generate, Shell};
 use clap_mangen::Man;
-use std::fs;
-use std::io;
 use std::io::Write;
+use std::{fs, io};
 
 #[derive(Debug, Args)]
 pub struct GenerateSubcommand {
     pub generate: Generate,
 
+    /// set output file path
     #[arg(short, long)]
     pub output: Option<std::path::PathBuf>,
+
+    /// generate complete for which shell
+    ///
+    /// if no set, use shell from env.
+    #[arg(short, long)]
+    pub shell: Option<Shell>,
 }
 
 #[derive(ValueEnum, Clone, Copy, Debug)]
@@ -20,12 +25,16 @@ pub enum Generate {
     /// generate manual doc
     Manual,
     /// generate sheel complete
-    Shell,
+    Complete,
 }
 
 fn generate_man_doc(output: &mut dyn Write) {
     let man = Man::new(Cli::command());
     man.render(output).unwrap();
+}
+
+fn generate_shell_complete(output: &mut dyn Write, shell: Shell) {
+    generate(shell, &mut Cli::command(), env!("CARGO_PKG_NAME"), output)
 }
 
 impl GenerateSubcommand {
@@ -37,7 +46,9 @@ impl GenerateSubcommand {
 
         match self.generate {
             Generate::Manual => generate_man_doc(output),
-            Generate::Shell => todo!(),
+            Generate::Complete => {
+                generate_shell_complete(output, self.shell.or_else(Shell::from_env).unwrap())
+            }
         }
     }
 }
